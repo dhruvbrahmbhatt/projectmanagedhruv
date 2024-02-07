f9e35e2ae8acbe6cee8c80fc21a33ad8bf2ad51a
unknown <dhruvbrahmbhatt5@gmail.com>
2024-02-07 15:29:26 +0530

    Fixed issue on notification error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,53 +22,58 @@
             <div
                 class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
             >
-                <a href="https://flowbite.com/"
-                    class="flex items-center space-x-3 rtl:space-x-reverse">
+                <a
+                    href="https://flowbite.com/"
+                    class="flex items-center space-x-3 rtl:space-x-reverse"
+                >
                     <span
                         class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
                         >PMS</span
                     >
                 </a>
+                @auth
                 <button
-                            id="dropdownDefaultButton"
-                            data-dropdown-toggle="dropdown"
-                            type="button"
+                    id="dropdownDefaultButton"
+                    data-dropdown-toggle="dropdown"
+                    type="button"
+                >
+                    <i class="fa fa-bell"></i>
+                    <span
+                        class="badge badge-light badge-xs"
+                        >{{auth()->user()->unreadNotifications->count()}}</span
+                    >
+                </button>
+                <!-- Dropdown menu -->
+                <ul
+                    id="dropdown"
+                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 p-1"
+                    aria-labelledby="dropdownDefaultButton"
+                >
+                    @if (auth()->user()->unreadNotifications)
+                    <li class="d-flex justify-content-end mx-1 my-2">
+                        <a
+                            href="{{ route('markAsRead') }}"
+                            class="btn btn-success btn-sm"
+                            >Mark All as Read</a
                         >
-                            <i class="fa fa-bell"></i>
-                            <span
-                                class="badge badge-light badge-xs"
-                                >{{auth()->user()->unreadNotifications->count()}}</span
-                            >
-                        </button>
-                        <!-- Dropdown menu -->
-                        <ul
-                            id="dropdown"
-                            class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 p-1"
-                            aria-labelledby="dropdownDefaultButton"
-                        >
-                            @if (auth()->user()->unreadNotifications)
-                            <li class="d-flex justify-content-end mx-1 my-2">
-                                <a
-                                    href="{{ route('markAsRead') }}"
-                                    class="btn btn-success btn-sm"
-                                    >Mark All as Read</a
-                                >
-                            </li>
-                            @endif @foreach (auth()->user()->unreadNotifications as $notification) 
-                            <a href="#"
-                                ><li class="p-1">
-                                    {{$notification->data['data']}}
-                                </li></a
-                            >
-                            @endforeach 
-                            @foreach(auth()->user()->readNotifications as $notification)
-                            <a href="#" class="text-secondary">
-                                <li class="p-1 text-secondary">
-                                    {{$notification->data['data']}}
-                                </li>
-                            </a>
-                            @endforeach
-                        </ul>
+                    </li>
+                    @endif @foreach (auth()->user()->unreadNotifications as
+                    $notification)
+                    <a href="#"
+                        ><li class="p-1">
+                            {{$notification->data['data']}}
+                        </li></a
+                    >
+                    @endforeach @foreach(auth()->user()->readNotifications as
+                    $notification)
+                    <a href="#" class="text-secondary">
+                        <li class="p-1 text-secondary">
+                            {{$notification->data['data']}}
+                        </li>
+                    </a>
+                    @endforeach
+                </ul>
+                @endauth
                 <button
                     data-collapse-toggle="navbar-default"
                     type="button"
@@ -93,38 +98,41 @@
                         />
                     </svg>
                 </button>
-                <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                <ul
-                    class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+                <div
+                    class="hidden w-full md:block md:w-auto"
+                    id="navbar-default"
                 >
-                    <li class="p-3">
-                        <a href="{{ route('home') }}">Home</a>
-                    </li>
-                    <li class="p-3">
-                        <a href="{{ route('dashboard') }}">Dashboard</a>
-                    </li>
-                    <li class="p-3">
-                        <a href="{{ route('tasks') }}">Tasks</a>
-                    </li>
-                    @auth
-                    <li class="p-3">
-                        <a href="">{{ auth()->user()->name }}</a>
-                    </li>
-                    <li class="p-3">
-                        <form action="{{ route('logout') }}" method="post">
-                            @csrf
-                            <button type="submit">Logout</button>
-                        </form>
-                    </li>
-                    @endauth @guest
-                    <li class="p-3">
-                        <a href="{{ route('login') }}">Login</a>
-                    </li>
-                    <li class="p-3">
-                        <a href="{{ route('register') }}">Register</a>
-                    </li>
-                    @endguest
-                </ul>
+                    <ul
+                        class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+                    >
+                        <li class="p-3">
+                            <a href="{{ route('home') }}">Home</a>
+                        </li>
+                        <li class="p-3">
+                            <a href="{{ route('dashboard') }}">Dashboard</a>
+                        </li>
+                        <li class="p-3">
+                            <a href="{{ route('tasks') }}">Tasks</a>
+                        </li>
+                        @auth
+                        <li class="p-3">
+                            <a href="">{{ auth()->user()->name }}</a>
+                        </li>
+                        <li class="p-3">
+                            <form action="{{ route('logout') }}" method="post">
+                                @csrf
+                                <button type="submit">Logout</button>
+                            </form>
+                        </li>
+                        @endauth @guest
+                        <li class="p-3">
+                            <a href="{{ route('login') }}">Login</a>
+                        </li>
+                        <li class="p-3">
+                            <a href="{{ route('register') }}">Register</a>
+                        </li>
+                        @endguest
+                    </ul>
                 </div>
             </div>
         </nav>
